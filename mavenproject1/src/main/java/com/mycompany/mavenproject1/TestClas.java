b1250e397c663767710614523247137c9daf8b43
Huỳnh Minh Quốc Nhật <40554847+hmqnhat@users.noreply.github.com>
2020-11-29 17:12:59 +0700

    tesst

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/TestClas.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author NhatQuoc
+ */
+public class TestClas {
+    private String keyword;
+    private String description;
+
+    public TestClas(String keyword, String description) {
+        this.keyword = keyword;
+        this.description = description;
+    }
+    
+
+    public String getKeyword() {
+        return keyword;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+ 
+    
+}
